ef54baacc821bd657802d6ab5cacc6b786873d62
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-09-28 20:25:47 -0400

    functioning game with procedural programming

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const boxArray = document.getElementsByClassName("box")
+let playerGoing = "X"
+
+for(box of boxArray){
+  box.addEventListener("click", function(e){
+    e.target.innerHTML = playerGoing
+    playerGoing === "X" ? playerGoing = "O" : playerGoing = "X"
+  })
+}
+
+console.log(boxArray)
